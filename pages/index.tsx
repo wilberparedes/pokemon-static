71ca87f66064b7504e0e35268cec0a32877d2aad
Wilber Paredes <wilberparedes@gmail.com>
2022-11-16 23:52:23 -0500

    feat: 40. config nextUiProvider and use this

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  return <h1>Hola Mundo!!!</h1>;
+  return (
+    <>
+      <h1>Hola Mundo!!!</h1>
+      <Button color="gradient">{`Hi I'm Button`}</Button>
+    </>
+  );
 };
 
 export default HomePage;
