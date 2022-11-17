da286df3c3e4b82e6421f512ba88b938263c7d4d
Wilber Paredes <wilberparedes@gmail.com>
2022-11-17 00:03:44 -0500

    feat: 41. config cssBaseline for nextUi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ import Document, {
   DocumentContext,
 } from "next/document";
 
+import { CssBaseline } from "@nextui-org/react";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const originalRenderPage = ctx.renderPage;
@@ -15,13 +17,13 @@ class MyDocument extends Document {
         enhanceComponent: (Component) => Component,
       });
     const initialProps = await Document.getInitialProps(ctx);
-    return initialProps;
+    return { ...initialProps, styles: <>{initialProps.styles}</> };
   }
 
   render() {
     return (
       <Html lang="es">
-        <Head></Head>
+        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
