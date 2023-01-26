d2f32254ccb49c8e14e83ec6dfbd50604becb49e
Wilber Paredes <wilberparedes@gmail.com>
2023-01-25 19:49:32 -0500

    66. Pantalla de Favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,19 @@
+import { Container, Text, Image } from "@nextui-org/react";
 import { Layout } from "../../components/layouts";
+import { NoFavorites } from "../../components/ui";
+import { useEffect, useState } from "react";
+import { localFavorites } from "../../utils";
 
 const FavoritesPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+  console.log("favoritePokemons", favoritePokemons);
+
   return (
     <Layout title="Favoritos">
-      <h1>Favorites Page</h1>
+      <NoFavorites />
     </Layout>
   );
 };
