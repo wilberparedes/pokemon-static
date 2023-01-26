7d25e03d45b28172cae53c0b177744f62b1c33ea
Wilber Paredes <wilberparedes@gmail.com>
2023-01-25 19:27:02 -0500

    63. guardar en localstorage

---
+++ utils/localFavorites.ts
@@ -0,0 +1,26 @@
+const toggleFavorite = (id: number) => {
+  console.log("toogle llamado");
+  let favorites: number[] = JSON.parse(
+    localStorage.getItem("favorites") || `[]`
+  );
+
+  if (favorites.includes(id)) {
+    favorites = favorites.filter((pokeId) => pokeId !== id);
+  } else {
+    favorites.push(id);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
+
+const existInFavorites = (id: number): boolean => {
+  if (typeof window === "undefined") return false;
+  const favorites: number[] = JSON.parse(
+    localStorage.getItem("favorites") || `[]`
+  );
+  return favorites.includes(id);
+};
+
+const exportedFunctions = { toggleFavorite, existInFavorites };
+
+export default exportedFunctions;
