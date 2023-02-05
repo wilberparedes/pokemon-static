3bdd3aee756b5c57c18a31c270d72e3066cf63b8
Wilber Paredes <wilberparedes@gmail.com>
2023-02-05 00:52:27 -0500

    74. Open Graph Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,8 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
   return (
     <>
@@ -18,6 +20,12 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
           content={`Información sobre el pokemon ${title}`}
         />
         <meta name="keywords" content={`pokemon, pokedex, ash, pikachu`} />
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
